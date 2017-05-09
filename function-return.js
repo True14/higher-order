3ac9a85d8262a94503a442fe1f3dbfb90c6e39c2
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-09 12:28:34 -0400

    Did stupid problem

---
+++ function-return.js
@@ -0,0 +1,21 @@
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter = 0;
+  return function(location){
+    warningCounter += 1;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}! `);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodWarning = hazardWarningCreator('Flood in your Area');
+const murderWarning = hazardWarningCreator('MURDER!!!');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+
+floodWarning('Main St and Pacific Ave');
+floodWarning("Centinela Ave and Olympic Blvd");
+
+murderWarning('Main St and Pacific Ave');
+murderWarning("Centinela Ave and Olympic Blvd");
